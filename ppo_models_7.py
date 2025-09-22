8f4cac6d0c910e7f1772296f8e7b582221ad1db3
J-Mara <jordan.maragakis@gmail.com>
2025-09-22 13:42:31 -0400

    second attempt to train ppo on cuda

--- ppo_models_7.py
+++ ppo_models_7.py
@@ -30,7 +30,8 @@ def pick_greedy_masked(logits: torch.Tensor, legal_mask: torch.Tensor) -> torch.
 
 def get_device() -> torch.device:
     try:
-        if torch.backends.mps.is_available():
+        #if torch.backends.mps.is_available():
+        if torch.cuda.is_available():
             return torch.device("cuda")
             #return torch.device("cpu")
     except Exception:
